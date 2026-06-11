dcb61edc71eb33cdbec2802a1813a2c1ad3eafbb
acutherapy <acuherb@yahoo.com>
2026-06-10 14:17:24 -1000

    add sms webhook

--- src/app/api/sms-webhook/route.ts
+++ src/app/api/sms-webhook/route.ts
@@ -1,1 +1,19 @@
-console.log("incoming sms", body);
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json({
+    success: true,
+    message: "sms webhook alive",
+  });
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  console.log("INCOMING SMS");
+  console.log(body);
+
+  return NextResponse.json({
+    success: true,
+  });
+}
