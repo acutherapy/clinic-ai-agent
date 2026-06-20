45e18e96152162337d5bcaaef079f1290fa5fbee
acutherapy <acuherb@yahoo.com>
2026-06-19 15:14:18 -1000

    restore ringcentral

--- src/lib/ringcentral.ts
+++ src/lib/ringcentral.ts
@@ -9,30 +9,14 @@ const rcsdk = new SDK({
 const platform =
   rcsdk.platform();
 
-let isLoggedIn = false;
-
-async function ensureLogin() {
-  if (isLoggedIn) {
-    return;
-  }
-
-  await platform.login({
-    jwt:
-      process.env.RINGCENTRAL_JWT!,
-  });
-
-  isLoggedIn = true;
-
-  console.log(
-    "RINGCENTRAL LOGIN SUCCESS"
-  );
-}
-
 export async function sendSMS(
   to: string,
   text: string
 ) {
-  await ensureLogin();
+  await platform.login({
+    jwt:
+      process.env.RINGCENTRAL_JWT!,
+  });
 
   let phone =
     to.replace(/\D/g, "");
@@ -69,19 +53,6 @@ export async function sendSMS(
         ],
         text,
       }
-    );
-
-  return await resp.json();
-}
-
-export async function getMessage(
-  messageId: string
-) {
-  await ensureLogin();
-
-  const resp =
-    await platform.get(
-      `/restapi/v1.0/account/~/extension/~/message-store/${messageId}`
     );
 
   return await resp.json();
